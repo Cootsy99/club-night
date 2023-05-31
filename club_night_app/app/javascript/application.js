cc32b00fb5d828c9f28b9b97faba0c9094ab59a0
Jack Coots <jack.coots@newtoneurope.com>
2023-05-31 15:50:54 +0100

    Fiddle with bootstrap installation

--- club_night_app/app/javascript/application.js
+++ club_night_app/app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
+//= require jquery-ui
+//= require jquery
 //= require bootstrap
+import * as bootstrap from "bootstrap"
