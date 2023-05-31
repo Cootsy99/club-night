1f2b77eadeaf3aed0d8ef6f841ef2d86b73bbac7
Jack Coots <jack.coots@newtoneurope.com>
2023-05-31 09:39:45 +0100

    migrate membership model

--- club_night_app/db/schema.rb
+++ club_night_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_082551) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_083906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_082551) do
     t.string "timetable"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "club_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "admin"
+    t.string "level"
+    t.string "membership_type"
+    t.date "membership_expiry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_id"], name: "index_memberships_on_club_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_082551) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "memberships", "clubs"
+  add_foreign_key "memberships", "users"
 end
