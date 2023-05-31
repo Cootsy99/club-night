dd8a73281c0b9daed3b245754355847e93be199b
Jack Coots <jack.coots@newtoneurope.com>
2023-05-31 10:02:34 +0100

    Edit club controller and index view

--- club_night_app/app/controllers/club_controller.rb
+++ club_night_app/app/controllers/club_controller.rb
@@ -1,2 +1,43 @@
 class ClubController < ApplicationController
+   before_action :set_club, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @clubs = Club.all
+    end
+
+    def show
+    end
+    
+    def new
+        @club = Club.new
+    end
+
+    def create
+        Club.create(club_params)
+        redirect_to clubs_path
+    end
+
+    def edit
+    end
+
+    def update
+        @club.update(club_params)
+        redirect_to @club
+    end
+
+    def destroy
+        @club.destroy
+        redirect_to clubs_path
+    end
+
+
+
+    private
+    def set_club
+        @club = Club.find(params[:id])
+    end
+
+    def club_params
+        params.require(:club).permit(:name, :founder, :location, :timetable)
+    end
 end
