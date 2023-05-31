f19b9bfe23ad4ee9c98a0dc4edde980f33fe8059
Jack Coots <jack.coots@newtoneurope.com>
2023-05-31 09:48:33 +0100

    Update user model Add dependency to club model

--- club_night_app/app/models/user.rb
+++ club_night_app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :memberships, dependent: :destroy
+  has_many :clubs, through: :memberships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
