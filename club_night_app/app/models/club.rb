141468b5e4a3eb004118316ee8c4796b2d49033b
Jack Coots <jack.coots@newtoneurope.com>
2023-05-31 14:34:12 +0100

    Edit sign up page

--- club_night_app/app/models/club.rb
+++ club_night_app/app/models/club.rb
@@ -2,7 +2,7 @@ class Club < ApplicationRecord
     has_many :memberships, dependent: :destroy
     has_many :users, through: :memberships
 
-    validates :name, :founder, :location, :timetable  presence: true
+    validates :name, :founder, :location, :timetable,  presence: true
   validates :name, uniqueness: true
 
 
