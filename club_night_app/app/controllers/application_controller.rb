c84fdf4366e8faaa32df82a4ced8597dbbc3a424
Jack Coots <jack.coots@newtoneurope.com>
2023-06-01 15:57:01 +0100

    Fix sign up issue

--- club_night_app/app/controllers/application_controller.rb
+++ club_night_app/app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
+  before_action :authenticate_user!  
   before_action :set_query
   before_action :set_user, only: %i[home]
 
   def home
-        @home  ="Club Night"
-        @memberships = Membership.where(user_id:  current_user.id)
-
-
+        if current_user.present?
+          @name = current_user.name
+          @memberships = Membership.where(user_id:  current_user.id)
+        else 
+          @memberships =[]
+        end
   end
 
   def set_query
@@ -16,6 +20,16 @@ class ApplicationController < ActionController::Base
 
   private
     def set_user
+      if current_user.present?
         @user = current_user
+      end 
+
     end
+
+    protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :name, :about, :club_id, :user_id, :admin, :level, :membership_type, :membership_expiry])
+  end
 end
+
--- club_night_app/app/models/membership.rb
+++ club_night_app/app/models/membership.rb
@@ -3,5 +3,7 @@ class Membership < ApplicationRecord
   belongs_to :user
 
   validates :admin, :level, presence: true
-
+  def self.membership_counter
+    self.count
+  end
 end
