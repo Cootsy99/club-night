3c85574f8e53590850fee9a82b706fe8ab7a7511
Jack Coots <jack.coots@newtoneurope.com>
2023-06-01 12:42:23 +0100

    Club tiles neatened

--- club_night_app/app/controllers/application_controller.rb
+++ club_night_app/app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :set_query
+  before_action :set_user, only: %i[home]
+
   def home
         @home  ="Club Night"
+        @memberships = Membership.where(user_id:  current_user.id)
+
+
   end
 
   def set_query
     @query = Club.ransack(params[:q])
   end
-  
+
+
+  private
+    def set_user
+        @user = current_user
+    end
 end
