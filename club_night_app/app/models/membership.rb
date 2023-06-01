d3a0fa1972a05347d2cb80eff0791fbd4a919865
Jack Coots <jack.coots@newtoneurope.com>
2023-06-01 16:32:12 +0100

    Add uniqueness validation to memberships model

--- club_night_app/app/models/membership.rb
+++ club_night_app/app/models/membership.rb
@@ -3,6 +3,8 @@ class Membership < ApplicationRecord
   belongs_to :user
 
   validates :admin, :level, presence: true
+  validates :user_id, uniqueness: { scope: :club_id, message: "You've already got a membership here!" }
+
   def self.membership_counter
     self.count
   end
