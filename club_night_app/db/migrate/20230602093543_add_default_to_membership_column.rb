a9bd49158e46767e5b9310cb6b85f9c0b010f5c9
Jack Coots <jack.coots@newtoneurope.com>
2023-06-02 10:36:34 +0100

    Run migration

--- club_night_app/db/migrate/20230602093543_add_default_to_membership_column.rb
+++ club_night_app/db/migrate/20230602093543_add_default_to_membership_column.rb
@@ -1,4 +1,5 @@
 class AddDefaultToMembershipColumn < ActiveRecord::Migration[7.0]
   def change
+    change_column :memberships, :present, :boolean, default: false
   end
 end
--- club_night_app/db/schema.rb
+++ club_night_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_093240) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_093543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_093240) do
     t.date "membership_expiry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "present"
+    t.boolean "present", default: false
     t.index ["club_id"], name: "index_memberships_on_club_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
   end
