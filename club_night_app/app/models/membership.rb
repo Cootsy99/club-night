77548cbeae2bf710bfc7b04d07d6f8fc9022f7f7
Jack Coots <jack.coots@newtoneurope.com>
2023-06-02 10:35:38 +0100

    Run migration

--- club_night_app/app/models/membership.rb
+++ club_night_app/app/models/membership.rb
@@ -5,7 +5,4 @@ class Membership < ApplicationRecord
   validates :admin, :level, presence: true
   validates :user_id, uniqueness: { scope: :club_id, message: "You've already got a membership here!" }
 
-  def self.membership_counter
-    self.count
-  end
 end
