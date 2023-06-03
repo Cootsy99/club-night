fd401916a1d7dd9e3f5b5d49556d6d3f35c702ee
Jack Coots <jack.coots@newtoneurope.com>
2023-06-03 13:14:07 +0100

    Remove players before create again

--- club_night_app/app/controllers/session_controller.rb
+++ club_night_app/app/controllers/session_controller.rb
@@ -16,6 +16,12 @@ class SessionController < ApplicationController
   end
 
   def random
+    @existing_players = Membership.where(club_id: params[:club_id], present: true, court: params[:court])
+    @existing_players.each do |player| 
+      player.waiting_to_play = true
+      player.court = 0
+      player.save
+    end
     @random_players = Membership.where(waiting_to_play: true, club_id: params[:club_id], present: true).order(Arel.sql('RANDOM()')).limit(4) 
     @random_players.each do |player| 
       player.waiting_to_play = false
