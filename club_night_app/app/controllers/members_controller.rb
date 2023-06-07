7818da4d52618d09f62195c8f519bb87b0e299fb
Jack Coots <jack.coots@newtoneurope.com>
2023-06-07 11:16:57 +0100

    Various Styling

--- club_night_app/app/controllers/members_controller.rb
+++ club_night_app/app/controllers/members_controller.rb
@@ -1,6 +1,7 @@
 class MembersController < ApplicationController
   def index
     @members = Club.find(params[:club_id]).users
+    @memberships = Club.find(params[:club_id]).memberships
     @club_name = params[:club_name]
     
   end
